cadf8c70e581d674dc8db648326f190564f96789
Izadora Borges <borgesizadora@gmail.com>
2022-11-06 18:14:10 -0300

    refactor(Home): refactored Home component to use react query lib

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,40 +1,63 @@
 import { useCallback, useEffect, useRef, useState } from 'react';
+import { useQuery } from 'react-query';
 
 import Loader from '~/components/Loader';
 import PokeList from '~/components/PokeList';
-import useFetchPokemons from '~/shared/hooks/useFetchPokemons';
+import { Result } from '~/models/Pokemon';
+import { getAllPokemon } from '~/services/Pokemon/pokemonRequests';
 
 const LIMIT = 20;
 
 function Home() {
   const [offset, setOffset] = useState(0);
+  const [allPokemonList, setAllPokemonList] = useState<Result[]>([]);
 
-  const { allPokemonList, loading } = useFetchPokemons(offset);
+  const { isFetching, isSuccess } = useQuery(
+    ['pokemonList', offset],
+    () => {
+      return getAllPokemon(offset, LIMIT);
+    },
+    {
+      onSuccess: (pokemonListData) => {
+        setAllPokemonList((prev) => [...prev, ...pokemonListData.results]);
+      }
+    }
+  );
+
+  const handleObserver = useCallback(
+    (entries: IntersectionObserverEntry[]) => {
+      const target = entries[0];
+
+      if (target.isIntersecting && isSuccess) {
+        setOffset((prev) => prev + LIMIT);
+      }
+    },
+    [isSuccess]
+  );
 
   const row = useRef(null);
-
-  const handleObserver = useCallback((entries: IntersectionObserverEntry[]) => {
-    const target = entries[0];
-    if (target.isIntersecting) {
-      setOffset((prev) => prev + LIMIT);
-    }
-  }, []);
 
   useEffect(() => {
     const option = {
       root: null,
       rootMargin: '20px',
-      threshold: 0
+      threshold: 1
     };
+    const rowCurrent = row.current;
+
     const observer = new IntersectionObserver(handleObserver, option);
-    if (row.current) observer.observe(row.current);
+    if (rowCurrent) observer.observe(rowCurrent);
+
+    return () => {
+      if (rowCurrent) observer.unobserve(rowCurrent);
+    };
   }, [handleObserver]);
 
   return (
     <div>
       {!!allPokemonList?.length && <PokeList pokemonList={allPokemonList} />}
       <div ref={row} />
-      {loading && <Loader />}
+      {isFetching && <Loader />}
     </div>
   );
 }
