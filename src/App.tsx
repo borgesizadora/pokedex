9b7718e8cf8ab72f78a10b4e3c1a2f95d9979235
Izadora Borges <borgesizadora@gmail.com>
2022-11-06 18:12:05 -0300

    chore(*): added react query library

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { QueryClientProvider, QueryClient } from 'react-query';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import { ThemeProvider } from 'styled-components';
@@ -11,23 +12,26 @@ import GlobalStyle from './shared/styles/global';
 import theme from './shared/styles/theme';
 
 function App() {
+  const queryClient = new QueryClient();
   return (
     <ThemeProvider theme={theme}>
-      <BrowserRouter>
-        <PageLayout>
-          <Routes>
-            <Route path="/">
-              <Route index element={<Home />} />
-              <Route path="pokedex" element={<Pokedex />}>
-                <Route path=":id" element={<PokemonId />} />
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <PageLayout>
+            <Routes>
+              <Route path="/">
+                <Route index element={<Home />} />
+                <Route path="pokedex" element={<Pokedex />}>
+                  <Route path=":id" element={<PokemonId />} />
+                </Route>
+                <Route path="pokemon/:id" element={<Pokemon />} />
+                <Route path="*" element={<div>Not found</div>} />
               </Route>
-              <Route path="pokemon/:id" element={<Pokemon />} />
-              <Route path="*" element={<div>Not found</div>} />
-            </Route>
-          </Routes>
-        </PageLayout>
-      </BrowserRouter>
-      <GlobalStyle />
+            </Routes>
+          </PageLayout>
+        </BrowserRouter>
+        <GlobalStyle />
+      </QueryClientProvider>
     </ThemeProvider>
   );
 }
