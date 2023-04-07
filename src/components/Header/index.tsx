5d6a5bcadc29d62e33aac2a802070737a18134c8
Izadora Borges <borgesizadora@gmail.com>
2023-04-07 19:42:56 -0300

    feat(Header): added hamburger menu for mobile

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,9 +2,12 @@ import { Link } from 'react-router-dom';
 
 import logo from '~/assets/pokemonLogo.svg';
 
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import * as NavigationMenu from '@radix-ui/react-navigation-menu';
+
 import { Container } from '../Container/styled';
 import * as S from './styles';
-
 const Header = () => {
   return (
     <S.Header>
@@ -14,7 +17,7 @@ const Header = () => {
             <img src={logo} alt="Pokemón logo" />
           </Link>
         </S.LogoWrapper>
-        <nav>
+        <S.HeaderNav>
           <S.LinksWrapper>
             <li>
               <Link to={'/'}>HOME</Link>
@@ -26,10 +29,48 @@ const Header = () => {
               <Link to={'/pokemon/1'}>POKEMON DATA</Link>
             </li>
           </S.LinksWrapper>
-        </nav>
+        </S.HeaderNav>
+        <MobileMenu />
       </Container>
     </S.Header>
   );
 };
 
 export default Header;
+
+export const MobileMenu = () => {
+  return (
+    <S.NavigationMenuRoot>
+      <S.NavigationMenuList>
+        <NavigationMenu.Item>
+          <S.NavMenuTrigger>
+            <FontAwesomeIcon icon={faBars} size={'xl'} />
+          </S.NavMenuTrigger>
+          <NavigationMenu.Content>
+            <S.NavigationMenuContentUL>
+              <S.NavigatioMenuLi>
+                <NavigationMenu.Link asChild>
+                  <Link to={'/'}>HOME</Link>
+                </NavigationMenu.Link>
+              </S.NavigatioMenuLi>
+              <S.NavigatioMenuLi>
+                <NavigationMenu.Link asChild>
+                  <Link to={'/pokedex/1'}>POKEDEX</Link>
+                </NavigationMenu.Link>
+              </S.NavigatioMenuLi>
+              <S.NavigatioMenuLi>
+                <NavigationMenu.Link asChild>
+                  <Link to={'/pokemon/1'}>POKEMON DATA</Link>
+                </NavigationMenu.Link>
+              </S.NavigatioMenuLi>
+            </S.NavigationMenuContentUL>
+          </NavigationMenu.Content>
+        </NavigationMenu.Item>
+      </S.NavigationMenuList>
+
+      <S.ViewportPosition>
+        <S.NavigationMenuViewport />
+      </S.ViewportPosition>
+    </S.NavigationMenuRoot>
+  );
+};
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,4 +1,28 @@
-import styled, { css } from 'styled-components';
+import styled, { css, keyframes } from 'styled-components';
+
+import * as NavigationMenu from '@radix-ui/react-navigation-menu';
+
+const scaleIn = keyframes`
+  from {
+    opacity: 0;
+    transform: rotateX(-30deg) scale(0.9);
+  }
+  to {
+    opacity: 1;
+    transform: rotateX(0deg) scale(1);
+  }
+`;
+
+const scaleOut = keyframes`
+ from {
+    opacity: 1;
+    transform: rotateX(0deg) scale(1);
+  }
+  to {
+    opacity: 0;
+    transform: rotateX(-10deg) scale(0.95);
+  }
+`;
 
 export const Header = styled.div`
   ${({ theme: { colors, spacing } }) => css`
@@ -46,4 +70,135 @@ export const LinksWrapper = styled.ul`
       }
     }
   `}
+`;
+
+export const HeaderNav = styled.nav`
+  ${({ theme: { breakpoints } }) => css`
+    @media (max-width: ${breakpoints.md}) {
+      display: none;
+    }
+  `}
+`;
+
+export const IconButton = styled.button`
+  ${({ theme: { colors, boxShadow } }) => css`
+    font-family: inherit;
+    border-radius: 100%;
+    height: 35px;
+    width: 35px;
+    display: inline-flex;
+    align-items: center;
+    justify-content: center;
+    color: ${colors.darkBlue};
+    background-color: ${colors.white};
+    box-shadow: ${boxShadow.dark};
+    border: none;
+  `}
+`;
+
+export const NavigationMenuRoot = styled(NavigationMenu.Root)`
+  ${({ theme: { breakpoints } }) => css`
+    display: flex;
+    justify-content: center;
+    z-index: 1;
+    @media (min-width: ${breakpoints.md}) {
+      display: none;
+    }
+  `}
+`;
+
+export const NavigationMenuList = styled(NavigationMenu.List)`
+  justify-content: center;
+  list-style: none;
+  margin: 0;
+`;
+
+export const NavMenuTrigger = styled(NavigationMenu.Trigger)`
+  ${({ theme: { colors } }) => css`
+    padding: 0.8rem;
+    border: none;
+    border-radius: 99px;
+    line-height: 0;
+    color: ${colors.darkBlue};
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  `}
+`;
+
+export const NavigationMenuContent = styled(NavigationMenu.Content)`
+  ${({ theme: { colors } }) => css`
+    border: 1px solid red;
+    height: 100vh;
+    opacity: 0.1;
+  `}
+`;
+
+export const NavMenuLink = styled(NavigationMenu.Link)`
+  padding: 8px 12px;
+  outline: none;
+  user-select: none;
+  font-weight: 500;
+  line-height: 1;
+  border-radius: 4px;
+  font-size: 15px;
+  color: red;
+  display: block;
+  text-decoration: none;
+  font-size: 15px;
+  line-height: 1;
+`;
+export const NavigationMenuContentUL = styled.ul`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  padding: 2rem 1rem;
+  gap: 1rem;
+`;
+export const NavigatioMenuLi = styled.li`
+  ${({ theme: { colors, boxShadow } }) => css`
+    border-radius: 8px;
+    background: ${colors.lightGray};
+    box-shadow: ${boxShadow.dark};
+    & > a {
+      height: 100%;
+      width: 100%;
+      padding: 1rem;
+      color: ${colors.darkBlue};
+      font-weight: 700;
+      display: block;
+    }
+  `}
+`;
+
+export const NavigationMenuViewport = styled(NavigationMenu.Viewport)`
+  ${({ theme: { colors, boxShadow } }) => css`
+    width: 100vw;
+
+    position: fixed;
+    left: -16px;
+    margin-top: 7px;
+    background-color: ${colors.mediumGray};
+    border-radius: 0 0 6px 6px;
+    overflow: hidden;
+    box-shadow: ${boxShadow.dark};
+    transition: width, height, 150ms ease;
+
+    &[data-state='open'] {
+      animation: ${scaleIn} 150ms ease;
+    }
+    &[data-state='closed'] {
+      animation: ${scaleOut} 150ms ease;
+    }
+  `}
+`;
+
+export const ViewportPosition = styled.div`
+  position: absolute;
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  top: 100%;
+  left: 0;
+  perspective: 2000px;
 `;
